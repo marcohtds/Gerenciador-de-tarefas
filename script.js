c938b6470dee542caa1b2597f55ce9e88da4c91b
marcohtds <marcoshenrtds@gmail.com>
2025-11-13 14:56:54 -0300

    Criação funções script.Js

--- script.js
+++ script.js
@@ -3,9 +3,67 @@ const apiUrl = 'http://localhost:3000/tasks';
 const form = document.getElementById("task-form");
 const taskList = document.getElementById('task-list');
 
-form.addEventListener('submit', async(e) => {
-        e.preventDefault();
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
-    const title = document.getElementById('title').values;
-    const description = document.getElementById('description').values;
-})
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+
+    try {
+        const res = await fetch(apiUrl, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title, description })
+        })
+        if (!res.ok) throw new Error("Erro ao adicionar tarefa");
+
+        const task = await res.json();
+        form.reset();
+        addTaskToUl(task);
+
+    } catch (error) {
+        alert("Error ao Salvar Tarefa" + error.message);
+    }
+});
+
+function addTaskToUl(task) {
+    const li = document.createElement("li");
+    li.className = task.completed ? "completed" : "";
+    li.innerHTML = `
+    <span>$(task.title) - ${task.description}</span>
+    <div>
+        <button onClick="toggleCompleted(${task.id}, ${task.completed})">✔️
+        </button>
+        <button onClick="deletTask(${task.id})">🗑️</button>
+        </div>
+    `;
+    taskList.appendChild(li);
+}
+
+async function loadTask() {
+    try {
+        const res = await fetch(apiUrl)
+        if (res.ok) throw new Error("Erro ao carregar tarefas");
+
+        const tasks = await res.json();
+        taskList.innerHTML = "";
+        task.forEach(addTaskToUl);
+
+    } catch (error) {
+        alert("Erro ao carregar tarefas: " + error.message);
+    }
+}
+
+async function toggleCompleted(id, completed) {
+    try {
+        await fetch(`${apiUrl}/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify{(completed: | completed)}
+        });
+loadTask();
+    } catch (error) {
+    alert("Erro ao atualizar tarefa: " + error.message);
+}
+    
+}
